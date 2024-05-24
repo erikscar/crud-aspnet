43fd4f5a9547c88b39ef834e2532093dc013c8f7
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-05-24 12:28:12 -0300

    Add Navbar Links for Providers and Departments and Add Providers Controller

---
+++ CRUD/Controllers/ProvidersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProvidersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
