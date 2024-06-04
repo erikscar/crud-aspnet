a102f3659a89b50231ac44be5c18b27462cb0112
erikscar <erikscarcela@gmail.com>
2024-06-04 16:17:02 -0300

    Added Aditional Valiidation for Data Field

--- CRUD/Models/Provider.cs
+++ CRUD/Models/Provider.cs
@@ -5,18 +5,27 @@ namespace CRUD.Models
     public class Provider
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(60, MinimumLength = 3,  ErrorMessage = "{0} Size Should be Between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [EmailAddress(ErrorMessage = "Enter a Valid Email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         [Display(Name = "Birth Date")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 5000.00, ErrorMessage = "{0} Must be From {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
+
 
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
