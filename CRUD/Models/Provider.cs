955e59c383b81519dd025d2d41868d4451e6c3af
erikscar <erikscarcela@gmail.com>
2024-06-04 12:16:55 -0300

    Added Annotation for Data Formatting

--- CRUD/Models/Provider.cs
+++ CRUD/Models/Provider.cs
@@ -1,12 +1,23 @@
-﻿namespace CRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
 {
     public class Provider
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<ProvidersRecord> Sales { get; set; } = new List<ProvidersRecord>();
