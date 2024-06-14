1201e7d9183cd93a82f4b9cc68789153adc59b22
erikscar <erikscarcela@gmail.com>
2024-06-14 13:49:58 -0300

    Adjust styles in Search pages

--- CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+242af010785936f92c19fbca59580b2424a205b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67dbab9697b89846c3fd143a52f8a581daa3020")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
